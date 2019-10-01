4494e7d9c4c183cc6c4d162273d6222c8e4d2b6e
Cera Smart <cera_smart@yahoo.com>
2019-09-30 21:27:38 -0500

    basic html and questions object added

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+//start screen
+
+//questions
+var questions = [{
+    question: "Botany is the study of?",
+    answers: ["Plants", "Rocks", "Planets", "Gorillas"],
+    correctAnswer: "Plants",
+    Image: "assets/images/plants.jpg",
+}, {
+    question: "Who is known as the 'father of botany'?",
+    answers: ["Mendel", "Aristotle", "Theophrastus", "Linnaeus"],
+    correctAnswer: "Theophrastus",
+    Image: "assets/images/Theophrastus.jpg",
+}, {
+    question: "How many seed leaves does a dicotyledon have?",
+    answers: ["0", "1", "2", "3"],
+    correctAnswer: "2",
+    Image: "assets/images/dicotyledon.jpg",
+}, {
+    question: "What is the botanical name for the male reporoductive organ in flowering plants?",
+    answers: ["Calyx", "Frond", "Carpel", "Stamen"],
+    correctAnswer: "Stamen",
+    Image: "assets/images/stamen.jpg",
+}, {
+    question: "What is the most poisonous plant in the world?",
+    answers: ["Narcissus", "Arican Daisy", "Poppy", "Orchid"],
+    correctAnswer: "Narcissus",
+    Image: "assets/images/narcissus.jpg",
+}, {
+    question: "What is the fastest growing plant in the world?",
+    answers: ["Cactus", "Carrots", "Bamboo", "Grass"],
+    correctAnswer: "Bamboo",
+    Image: "assets/images/bamboo.jpg",
+}, {
+    question: "What gives Carrots their orange color?",
+    answers: ["Chlorophyll", "Beta-carotene", "Carotene", "Vitamin A"],
+    correctAnswer: "Beta-carotene",
+    Image: "assets/images/beta-carotene.jpg",
+}];
